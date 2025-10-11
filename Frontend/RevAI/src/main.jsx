3144bf52bec4ab10778ca2afb0a3d820d9a8acef
roshanishrestha084@gmail.com <roshanishrestha084@gmail.com>
2025-10-11 20:43:25 +0545

    Login error fix in Frontend

--- Frontend/RevAI/src/main.jsx
+++ Frontend/RevAI/src/main.jsx
@@ -26,7 +26,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/LoginPage" element={<Login/>} />
+        <Route path="/LoginPage" element={<LoginPage/>} />
         </Route>
       </Routes>
     </BrowserRouter>
